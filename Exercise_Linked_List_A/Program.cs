2f27bdac92c5dd8ae1d8506485d747d3497c10f7
Chendy Lundy Janitra Putra Ashari <chendy.lundy.ft21@mail.umy.ac.id>
2022-11-23 15:42:57 +0700

    add class Node

---
+++ Exercise_Linked_List_A/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Linked_List_A
+{
+    internal class Program
+    {
+        class Node
+        {
+            public int rollNumber;
+            public string name;
+            public Node next;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
